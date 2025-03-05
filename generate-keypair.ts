dfc47593ded956cb1ae13a272e84d7eeb3bcaa22
efirus09 <efirus09@gmail.com>
2025-03-05 23:12:05 +0000

    Write secret_key to .env file

--- generate-keypair.ts
+++ generate-keypair.ts
@@ -4,4 +4,13 @@ const keypair = Keypair.generate();
 
 console.log(`The public key is: `, keypair.publicKey.toBase58());
 console.log(`The secret key is: `, keypair.secretKey);
+
+fs.open('.env', 'w', (err) => {
+        if(err) throw err;
+        fs.appendFile('.env', `SECRET_KEY="${keypair.secretKey}"`, (err) => {
+        	if(err) throw err;
+        	console.log('File .env created!');
+   		});
+	});
+
 console.log(`✅ Finished!`);
